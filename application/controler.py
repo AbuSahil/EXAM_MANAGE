e395421ee48416c1c927ec672dbeda586a9fe020
Abu Sahil <abusa16198@gmail.com>
2026-07-12 23:41:33 +0530

    add marks added

--- application/controler.py
+++ application/controler.py
@@ -30,26 +30,40 @@ def login():
             if this_user.role== 'admin':
                 if this_user.password==password:
                     login_user(this_user)
-                    return render_template("admin/admin_dashboard.html")
+                    return redirect("/admin")
                 return "<h1>Incorect Password</h1>"
             elif this_user.role == 'staff':
                 if this_user.password ==password:
                     login_user(this_user)
-                    return render_template("staff/staff_dashboard.html")
+                    return redirect("/staff")
                 return "<h1>Incorect Password</h1>"
             elif this_user.role =='student':
                 if this_user.password == password:
                     login_user(this_user)
-                    return render_template("student/student_dashboard.html")
+                    return redirect("/student")
                 return "<h1>Incorect Password</h1>"
 
         return "<h1> No User Registerd with this Email</h1>"
     return render_template("home.html")
+@app.route("/admin")
+@login_required
+def admin():
+    return render_template("admin/admin_dashboard.html")
+@app.route("/staff")
+@login_required
+def staff():
+
+    return render_template("staff/staff_dashboard.html" , staff=current_user)
+@app.route("/student")
+@login_required
+def student():
+    return render_template("student/student_dashboard.html")
+
 @app.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect("/login")
+    return redirect("/")
 
 @app.route("/retrieve_password" , methods = ['POST','GET'])
 def retrieve_password():
@@ -60,9 +74,254 @@ def retrieve_password():
           return render_template("retrieve.html" , password=password.password)
       return render_template("retrieve.html" , password=" No Password found ")
     return render_template("forgot.html")
-       
 
 
+@app.route("/add_staff", methods=["GET", "POST"])
+@login_required
+def add_staff():
+    subjects = Subject.query.all()
+
+    if request.method == "POST":
+
+        name = request.form.get("name")
+        employee_id = request.form.get("employee_id")
+        email = request.form.get("email")
+        phone = request.form.get("phone")
+        password = request.form.get("password")
+        designation = request.form.get("designation")
+        subject_id = request.form.get("subject")
+
+        # Check email
+        existing_user = User.query.filter_by(email=email).first()
+
+        if existing_user:
+            flash("Email already exists.", "danger")
+            return redirect(url_for("add_staff"))
+
+        # Check employee id
+        existing_staff = Staff.query.filter_by(employee_id=employee_id).first()
+
+        if existing_staff:
+            flash("Employee ID already exists.", "danger")
+            return redirect(url_for("add_staff"))
+
+        # Create User
+        user = User( role="staff", name=name, email=email, password=password)
+
+            # Create Staff Profile
+        staff = Staff( employee_id=employee_id, designation=designation, subject_id=subject_id , phone=phone )
+
+            # Link Staff with User
+        staff.user = user
+
+        db.session.add(staff)
+        db.session.commit()
+
+        flash("Staff added successfully.", "success")
+        return redirect(url_for("staff_list"))
+
+        
+    
+    return render_template("admin/add_staff.html", subjects=subjects)
+@app.route("/staff_list")
+@login_required
+def staff_list():
+    staffs=Staff.query.all()
+
+    return render_template("admin/staff_list.html" , staffs=staffs)
+@app.route("/activate_staff/<int:staff_id>", methods=["POST"])
+def activate_staff(staff_id):
+    
+
+    staff = Staff.query.get(staff_id)
+    if staff:
+        staff.user.is_active = True
+        db.session.commit()
+        flash("Staff activated successfully.", "success")
+
+    return redirect(url_for("staff_list"))
+@app.route("/deactivate_staff/<int:staff_id>", methods=["POST"])
+def deactivate_staff(staff_id):
+    
+
+    staff = Staff.query.get(staff_id)
+    if staff:
+        staff.user.is_active = False
+        db.session.commit()
+        flash("Staff deactivated successfully.", "warning")
+
+    return redirect(url_for("staff_list"))
+
+@app.route("/add_student", methods=["GET", "POST"])
+@login_required
+def add_student():
+    classes=SchoolClass.query.all()
+
+    if request.method == "POST":
+
+        name = request.form.get("name")
+        roll_no = request.form.get("roll_no")
+        email = request.form.get("email")
+        phone = request.form.get("phone")
+        password = request.form.get("password")
+        class_id = request.form.get("class_name")
+        
+
+        # Check email
+        existing_user = User.query.filter_by(email=email).first()
+
+        if existing_user:
+            flash("Email already exists.", "danger")
+            return redirect(url_for("add_student"))
+
+        # Check employee id
+        existing_student = Student.query.filter_by(roll_no=roll_no).first()
+
+        if existing_student:
+            flash("Roll No already exists.", "danger")
+            return redirect(url_for("add_student"))
+
+        # Create User
+        user = User( role="student", name=name, email=email, password=password)
+
+            # Create Student Profile
+        student = Student( roll_no=roll_no, class_id=class_id, phone=phone )
+
+            # Link Staff with User
+        student.user = user
+
+        db.session.add(student)
+        db.session.commit()
+
+        flash("Student added successfully.", "success")
+        return redirect(url_for("student_list"))
+
+        
+
+    return render_template("admin/add_student.html", classes=classes)
+
+@app.route("/student_list")
+@login_required
+def student_list():
+    students=Student.query.all()
+
+    return render_template("admin/student_list.html" , students=students)
+@app.route("/activate_student/<int:student_id>", methods=["POST"])
+def activate_student(student_id):
+    
+
+    student = Student.query.get(student_id)
+    if student:
+        student.user.is_active = True
+        db.session.commit()
+        flash("Student activated successfully.", "success")
+
+    return redirect(url_for("student_list"))
+@app.route("/deactivate_student/<int:student_id>", methods=["POST"])
+def deactivate_student(student_id):
+    
+
+    student = Student.query.get(student_id)
+    if student:
+        student.user.is_active = False
+        db.session.commit()
+        flash("student deactivated successfully.", "warning")
+
+    return redirect(url_for("student_list"))
+
+@app.route("/create_exam", methods=["GET", "POST"])
+@login_required
+def create_exam():
+
+    subjects = Subject.query.order_by(Subject.name).all()
+    staffs = Staff.query.order_by(Staff.id).all()
+
+    if request.method == "POST":
+
+        exam = Exam(
+            name=request.form.get("name"),
+            subject_id=request.form.get("subject_id"),
+            exam_date=datetime.strptime(
+                request.form.get("exam_date"),
+                "%Y-%m-%d"
+            ).date(),
+            total_marks=request.form.get("total_marks"),
+            pass_marks=request.form.get("pass_marks"),
+            staff_id=request.form.get("staff_id")
+        )
+
+        db.session.add(exam)
+        db.session.commit()
+
+        flash("Exam created successfully.", "success")
+
+        return redirect(url_for("create_exam"))
+
+    return render_template(
+        "admin/create_exam.html",
+        subjects=subjects,
+        staffs=staffs
+    )
+
+@app.route("/add_subject", methods=["GET", "POST"])
+def add_subject():
+    classes = SchoolClass.query.all()
+    subjects = Subject.query.order_by(Subject.class_id).all()
 
 
+    if request.method == "POST":
 
+        name = request.form.get("name").strip()
+        class_id = request.form.get("class_name")
+
+        subject = Subject.query.filter_by(
+            name=name,
+            class_id=class_id
+        ).first()
+
+        if subject:
+            flash("Subject already exists.", "warning")
+            return redirect(url_for("add_subject"))
+
+        subject = Subject(
+            name=name,
+            class_id=class_id
+        )
+
+        db.session.add(subject)
+        db.session.commit()
+
+        flash("Subject added successfully.", "success")
+
+        return redirect(url_for("add_subject"))
+
+    return render_template("admin/add_subject.html" , subjects=subjects ,classes= classes)
+@app.route("/delete_subject/<int:subject_id>", methods=["POST"])
+@login_required
+def delete_subject(subject_id):
+
+    subject = Subject.query.get_or_404(subject_id)
+
+    db.session.delete(subject)
+    db.session.commit()
+
+    flash("Subject deleted successfully.", "success")
+
+    return redirect(url_for("add_subject"))
+
+@app.route("/manage_exams")
+@login_required
+def manage_exams():
+    exams = Exam.query.all()
+    return render_template("staff/manage_exams.html", exams=exams , staff=current_user)
+
+@app.route("/add_marks/<int:exam_id>")
+@login_required
+def add_marks(exam_id):
+    exam = Exam.query.get_or_404(exam_id)
+
+    students = Student.query.filter_by(
+    class_id=exam.subject.class_id
+).order_by(Student.roll_no).all()
+
+    return render_template("staff/add_marks.html" , exam = exam, students=students , staff=current_user)
--- application/model.py
+++ application/model.py
@@ -1,98 +1,201 @@
 from .database import db
-from datetime import datetime ,date
 from flask_login import UserMixin
+from datetime import datetime
 
+
+# ==========================
+# User
+# ==========================
 class User(db.Model, UserMixin):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    role= db.Column(db.String(), nullable=False)
-    name = db.Column(db.String(30), nullable=False)
-    email = db.Column(db.String(30), nullable=False)
-    password = db.Column(db.String(), nullable=False)
+
+    role = db.Column(db.String(20), nullable=False)       # Admin / Staff / Student
+    name = db.Column(db.String(100), nullable=False)
+
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
     is_active = db.Column(db.Boolean, default=True)
 
-    staff = db.relationship("Staff", back_populates="user", uselist=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-    student = db.relationship("Student",back_populates="user", uselist=False )
+    # Relationships
+    staff = db.relationship(
+        "Staff",
+        back_populates="user",
+        uselist=False,
+        cascade="all, delete-orphan"
+    )
+
+    student = db.relationship(
+        "Student",
+        back_populates="user",
+        uselist=False,
+        cascade="all, delete-orphan"
+    )
+
+
+# ==========================
+# Staff
+# ==========================
 class Staff(db.Model):
-    __tablename__ = 'staffs' 
-    id =  db.Column(db.Integer , primary_key = True)
-    user_id = db.Column(db.Integer , db.ForeignKey("users.id") , unique =  True)
-    employee_id = db.Column(db.String(30), unique = True)
-    designation = db.Column(db.String())
-    subject = db.Column(db.String())
-    # relationShip with other Table
-    schedules = db.relationship("ExamSchedule", back_populates="staff")
-    user = db.relationship("User",back_populates="staff")
+    __tablename__ = "staffs"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"),unique=True,nullable=False)
+
+    employee_id = db.Column(db.String(30),unique=True, nullable=False )
+    subject_id = db.Column(db.Integer,db.ForeignKey("subjects.id"))
+
+    designation = db.Column(db.String(50), nullable=False)
+    phone = db.Column(db.String(15))
+
+    # Relationships
+    user = db.relationship("User", back_populates="staff")
+    subject = db.relationship("Subject" , back_populates="staff")
+    exams = db.relationship("Exam",back_populates="staff", cascade="all, delete-orphan")
+
+
+# ==========================
+# Student
+# ==========================
+class Student(db.Model):
+    __tablename__ = "students"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"), unique=True, nullable=False )
+
+    roll_no = db.Column(db.String(20), nullable=False)
+
+    class_id = db.Column(
+    db.Integer,
+    db.ForeignKey("classes.id"),
+    nullable=False
+)
+
     
 
-class Student(db.Model):
-    __tablename__ = 'students'
-    id =  db.Column(db.Integer , primary_key = True)
-    user_id = db.Column(db.Integer , db.ForeignKey("users.id") , unique =  True)
-    roll_no = db.Column(db.String(),unique = True)
-    class_name = db.Column(db.String())
-    section = db.Column(db.String())
-    # RelationShip with other table
-    user = db.relationship("User",back_populates="student")
-    marks = db.relationship("Mark", back_populates="student")
+    phone = db.Column(db.String(15))
 
-class AcademicYear(db.Model):
-    __tablename__ = 'academic_year'
-    id =  db.Column(db.Integer , primary_key = True)
-    year = db.Column(db.String())
-    is_active = db.Column(db.Boolean , default = False)
+    # Relationships
+    user = db.relationship("User", back_populates="student")
 
+    marks = db.relationship("Mark",back_populates="student",cascade="all, delete-orphan")
+    school_class = db.relationship("SchoolClass",back_populates="students")
+    __table_args__ = (
+        db.UniqueConstraint(
+            "roll_no",
+            "class_id",
+            
+            name="unique_roll_per_section"
+        ),
+    )
+
+class SchoolClass(db.Model):
+    __tablename__ = "classes"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(20), nullable=False)
+    section = db.Column(db.String(10), nullable=False)
+
+    # Relationships
+    students = db.relationship(
+        "Student",
+        back_populates="school_class",
+        cascade="all, delete-orphan"
+    )
+
+    subjects = db.relationship(
+        "Subject",
+        back_populates="school_class",
+        cascade="all, delete-orphan"
+    )
+
+    __table_args__ = (
+        db.UniqueConstraint(
+            "name",
+            "section",
+            name="unique_class_section"
+        ),
+    )
+
+    def __repr__(self):
+        return f"{self.class_name} - {self.section}"
+# ==========================
+# Subject
+# ==========================
+class Subject(db.Model):
+    __tablename__ = "subjects"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(50), nullable=False)
+
+    class_id = db.Column(
+        db.Integer,
+        db.ForeignKey("classes.id"),
+        nullable=False
+    )
+
+    # Relationships
+    school_class = db.relationship(
+        "SchoolClass",
+        back_populates="subjects"
+    )
+
+    exams = db.relationship(
+        "Exam",
+        back_populates="subject",
+        cascade="all, delete-orphan"
+    )
+
+    staff = db.relationship(
+        "Staff",
+        back_populates="subject"
+    )
+# ==========================
+# Exam
+# ==========================
 class Exam(db.Model):
-    __tablename__ = 'exams'
-    id =  db.Column(db.Integer , primary_key = True)
-    name = db.Column(db.String())
-    academic_year_id = db.Column(db.Integer , db.ForeignKey("academic_year.id"))
-    schedules = db.relationship("ExamSchedule",back_populates="exam")
+    __tablename__ = "exams"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    subject_id = db.Column(db.Integer,  db.ForeignKey("subjects.id"),nullable=False )
+    exam_date = db.Column(db.Date, nullable=False)
+    total_marks = db.Column(db.Integer, nullable=False)
+    pass_marks = db.Column(db.Integer, nullable=False)
+    staff_id = db.Column(db.Integer,db.ForeignKey("staffs.id"),nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    # Relationships
+    subject = db.relationship("Subject", back_populates="exams")
+    staff = db.relationship("Staff", back_populates="exams")
+    marks = db.relationship("Mark",back_populates="exam",cascade="all, delete-orphan")
 
-class ExamSchedule(db.Model):
-    __tablename__ = 'exam_schedule'
-    id =  db.Column(db.Integer , primary_key = True)
-    exam_id = db.Column(db.Integer, db.ForeignKey('exams.id'))
-    subject  = db.Column(db.String())
-    exam_date = db.Column(db.Date )
-    total_marks = db.Column(db.Integer)
-    pass_marks = db.Column(db.Integer)
-    staff_id = db.Column(db.Integer , db.ForeignKey("staffs.id"))
-    # relationShip
-    exam = db.relationship("Exam", back_populates='schedules')
-    staff = db.relationship("Staff", back_populates='schedules')
-    marks = db.relationship("Mark",  back_populates="schedule")
 
+# ==========================
+# Marks
+# ==========================
 class Mark(db.Model):
-    __tablename__ = 'marks'
-    id =  db.Column(db.Integer , primary_key = True)
-    student_id = db.Column(db.Integer , db.ForeignKey("students.id"))
-    exam_schedule_id = db.Column(db.Integer , db.ForeignKey("exam_schedule.id"))
-    marks_obtained = db.Column(db.Float)
+    __tablename__ = "marks"
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer,db.ForeignKey("students.id"),nullable=False )
+    exam_id = db.Column(db.Integer,db.ForeignKey("exams.id"), nullable=False )
+    marks_obtained = db.Column(db.Float, nullable=False)
     grade = db.Column(db.String(5))
     remarks = db.Column(db.String(100))
-    # relationShip
-
-    student = db.relationship("Student", back_populates = 'marks')
-    schedule = db.relationship("ExamSchedule" , back_populates = 'marks')
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-       
-
-
-
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    # Relationships
+    student = db.relationship("Student", back_populates="marks")
+    exam = db.relationship("Exam", back_populates="marks")
+    __table_args__ = (
+        db.UniqueConstraint(
+            "student_id",
+            "exam_id",
+            name="unique_student_exam"
+        ),
+    )
