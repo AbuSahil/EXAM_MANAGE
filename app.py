5546573a059508e6ca5e666679fce1684fba927c
Abu Sahil <abusa16198@gmail.com>
2026-07-16 23:50:41 +0530

    depl

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def create_app():
     
     app.config['SECRET_KEY'] = 'qwertyuioplkjhgfdsazxcvbnm'
     app.config["SQLALCHEMY_DATABASE_URI"] = \
-    "postgresql://postgres:BJV2026@localhost:5432/school_management"
+    "postgresql://schooladmin:BJV2026@localhost:5432/school_management"
 
     db.init_app(app)
     
